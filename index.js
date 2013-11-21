4ed5de3bda77ae9b8d13ebc6e605aef833fe06bc
doclements <d.o.clements@googlemail.com>
2013-11-21 15:50:03 +0000

    Update index.js
    added a grunt specific init so you can pass the done() promise object in - allows this library to be used by grunt-doccy

--- index.js
+++ index.js
@@ -8,24 +8,44 @@ var fs = require("fs");
  */
 var doccy = {
   /*
+   * @name init_grunt
+   * @param filename the src file to read for docstrings
+   * @param output the file name to output markdown into
+   * @param done the Grunt promise object to call when finished
+   */
+  init_grunt: function(filename, output, _done) {
+    //console.log('----',filename);
+    //console.log('+++++', output);
+    fs.readFile(filename, "utf-8",  function(err, contents) {
+      if(err) {this.logg(err);}
+      var parsed = parseFile.parse(contents);
+      var markdown = writeMarkdown.parse(parsed);
+      this.writeToFile(markdown, output, _done);
+    }.bind(this));
+    
+  },
+  /*
    * @name init
    * @param filename the src file to read for docstrings
    * @param output the file name to output markdown into
    */
   init: function(filename, output) {
-    fs.readFile(filename, "utf8", function(err, contents) {
-      if(err) console.log(err);
+    //console.log('----',filename);
+    //console.log('+++++', output);
+    fs.readFile(filename, "utf-8",  function(err, contents) {
+      if(err) {this.logg(err);}
       var parsed = parseFile.parse(contents);
       var markdown = writeMarkdown.parse(parsed);
       this.writeToFile(markdown, output);
     }.bind(this));
+    
   },
   /*
    * @name writeToFile
    * @param markdown the result of calling writeMarkdown.parse
    * @param output the filename to output to
    */
-  writeToFile: function(markdown, output) {
+  writeToFile: function(markdown, output, _done) {
     var toWrite = [];
     markdown.forEach(function(block) {
       toWrite.push(block.join("\n\n"));
@@ -34,6 +54,11 @@ var doccy = {
     fs.writeFile(output, contents, function(err) {
       if(err) console.log(err);
       console.log("File " + output + " written.");
+      if (_done) {
+       console.log('exectuing DONE');
+        _done();
+      }
+      
     });
   }
 };
